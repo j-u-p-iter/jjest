a07b1537f67923a2770bff533c79b913120579bb
j.u.p.iter <pn79122821224@gmail.com>
2020-10-21 21:20:58 +0500

    feat(test runner): continue extending test runner

--- src/Reporter.ts
+++ src/Reporter.ts
@@ -1,21 +1,15 @@
-// https://github.com/twosmalltrees/pretzel-test/blob/master/src/reporter.ts
-//
-// Listen events and log appropriate message
-// So, there's a class specially for reporting and we
-// don't have console.log in multiple places of the module
-
 import { EventManager } from "./EventManager";
 
-export class Reporter {
-  constructor(private eventManager: EventManager) {}
+export const Reporter = () => {
+  const [tests, setTests] = useState([]);
 
-  init() {
-    this.eventManager.emit("testSuiteStarted", () => {
-      console.log("testSuiteStarted");
-    });
+  useEffect(() => {
+    this.eventManager.emit("testSuiteStarted", () => {});
 
     this.eventManager.on("testSuiteCompleted", () => {
       console.log("testSuiteCompleted");
     });
-  }
-}
+  }, []);
+
+  return null;
+};
