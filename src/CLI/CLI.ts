de7e7aa82ac079db0adc35366c388a192aa8ff7f
Kirill <pn79122821224@gmail.com>
2020-11-08 12:06:21 +0500

    feat(cli): handles errors properly

--- src/CLI/CLI.ts
+++ src/CLI/CLI.ts
@@ -1,23 +1,23 @@
-import { Command } from 'commander';
+import { Command } from "commander";
 
-import { RunCommand } from './RunCommand';
-import { InitCommand } from './InitCommand';
-import { Trun } from '../Trun';
+import { Trun } from "../Trun";
+import { InitCommand } from "./InitCommand";
+import { RunCommand } from "./RunCommand";
 
 export enum CommandName {
-  RUN = 'run',
-  INIT = 'init',
-};
+  RUN = "run",
+  INIT = "init"
+}
 
 export class CLI {
   private getVersion() {
-    const { version } = require('package.json');
-    
+    const { version } = require("package.json");
+
     return version;
   }
 
   private runCommand(commandName) {
-    switch(commandName) {
+    switch (commandName) {
       case CommandName.RUN:
         new RunCommand().execute();
 
@@ -27,7 +27,7 @@ export class CLI {
   }
 
   /**
-   * Run - is the default command, it executes at the end if 
+   * Run - is the default command, it executes at the end if
    *   there is no any another method to execute.
    */
   public run() {
@@ -36,20 +36,32 @@ export class CLI {
      *   on each action of the program, that is not possible to do with simple if else.
      */
     return new Promise((resolve, reject) => {
+      const callIt = async callback => {
+        try {
+          const result = await callback();
+
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      };
+
       const program = new Command();
-      
+
       // sets up the CLI version
       program.version(this.getVersion());
-      
-      program
-        .command('init')  
-        .description('Creates a default trun.json config file')
-        .action(resolve(() => runCommand(CommandName.INIT)));
-      
-      program
-        .option('-w, --watch', 'Enables watching mode (rerun tests on change the tests or the main code)');
 
-      resolve(() => runCommand(CommandName.RUN));
+      program
+        .command("init")
+        .description("Creates a default trun.json config file")
+        .action(callIt(() => runCommand(CommandName.INIT)));
+
+      program.option(
+        "-w, --watch",
+        "Enables watching mode (rerun tests on change the tests or the main code)"
+      );
+
+      callIt(() => runCommand(CommandName.RUN));
     });
   }
-} 
+}
