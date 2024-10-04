9b711ba1eb48e7dde2a985767144352b67bda96b
1234 <a1234@1234s-MacBook-Pro.local>
2024-10-04 08:36:14 +0200

    continues moving package to the monorepo

---
+++ packages/cli/src/InitCommand.ts
@@ -0,0 +1,25 @@
+import { Command as Program } from 'commander';
+
+import { TrunConfig } from "@j.u.p.iter/jtrun-config";
+
+import { Command, CommandName } from "./Command.js";
+
+export class InitCommand extends Command {
+  constructor(private program: Program) {
+    super();
+  }
+
+  public name = CommandName.INIT;
+
+  public description = "Creates a default trun.json config file";
+
+  public initialize() {
+    this.program.command(this.name).description(this.description);
+
+    return this;
+  }
+
+  public execute() {
+    new TrunConfig().create();
+  }
+}
---
+++ packages/cli/src/RunCommand.ts
@@ -0,0 +1,83 @@
+import { Command as Program } from 'commander';
+import { Trun } from "@j.u.p.iter/jtrun-trun";
+import { TrunConfig, TSConfig, TrunConfigOptions } from "@j.u.p.iter/jtrun-config";
+
+import { Command } from "./Command.js";
+
+/**
+ * This is default command. It's used to run tests.
+ *
+ */
+export class RunCommand extends Command {
+  private validOptions = ["watch", "config", "tsconfig"] as const;
+
+  private trun = new Trun();
+
+  private trunConfig: TrunConfigOptions | null = null;
+
+  private commandOptions: any;
+
+  private async runTests() {
+    const tsConfig = await new TSConfig(this.resolveOption("tsconfig")).load();
+
+    this.trun.run(tsConfig, this.resolveOptions());
+  }
+
+  private resolveOption(optionName: Exclude<typeof this.validOptions[number], 'config'>) {
+    return this.commandOptions[optionName] || this.trunConfig![optionName];
+  }
+
+  private resolveOptions() {
+    return { ...this.trunConfig, ...this.commandOptions };
+  }
+
+  private readCommandOptions() {
+    this.commandOptions = this.prepareOptions(this.program, this.validOptions);
+  }
+
+  private async readConfig() {
+    this.trunConfig = await new TrunConfig(this.commandOptions.config).load();
+  }
+
+  constructor(private program: Program) {
+    super();
+  }
+
+  public initialize() {
+    this.program
+      .option(
+        "-w, --watch",
+        "Enables watching mode (rerun tests on change the tests or the main code)"
+      )
+      .option(
+        "-c, --config <pathToConfig>",
+        "Sets up a file's path to a config"
+      )
+      .option(
+        "-ts, --tsconfig <pathToTSConfig>",
+        "Sets up a file's path to a typescript config"
+      );
+
+    return this;
+  }
+
+  public async execute() {
+    /**
+     * Options can be passed:
+     *
+     * 1. With CLI.
+     * 2. Set up in the config.
+     *
+     */
+    this.readCommandOptions();
+    await this.readConfig();
+
+    if (this.resolveOption("watch")) {
+      /**
+       * Rerun trun on file changes
+       */
+    } else {
+      this.runTests();
+    }
+  }
+}
