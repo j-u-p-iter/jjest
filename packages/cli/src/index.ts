7d5510113fbefc28af5411ba2f19b5cc78c23820
1234 <a1234@1234s-MacBook-Pro.local>
2024-10-04 08:59:31 +0200

    continues moving package to the monorepo

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,15 @@
-export * from './CLI.js';
+#!/usr/bin/env node
+
+import { CLI } from "./CLI.js";
+
+const cli = new CLI();
+
+const run = async () => {
+  try {
+    await cli.run();
+  } catch(error) {
+    console.log("Unhandled error caught:", error);
+  }
+};
+
+run();
